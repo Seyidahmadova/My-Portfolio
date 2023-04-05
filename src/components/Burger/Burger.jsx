8d1a36cc5f7bb6dd67c4b2ea35523f1dac2d979a
Aysel Seyidahmadova <romaseyidahmedov@gmail.com>
2023-04-06 00:43:49 +0400

    Added Burger Menu

---
+++ src/components/Burger/Burger.jsx
@@ -0,0 +1,52 @@
+import React, {useState} from 'react';
+import Nav from 'react-bootstrap/Nav';
+import './Burger.css';
+
+export default function Burger() {
+
+ const [burger_class, setBurgerClass] = useState('burger-bar unclicked')
+ const [menu_class, setMenuClass] = useState('menu hidden')
+ const [isMenuClicked, setMenuClicked] = useState(false)
+
+
+ const updateMenu = () => {
+    if(!isMenuClicked){
+      setBurgerClass('burger-bar clicked')
+      setMenuClass('menu visible')
+    }
+    else{
+      setBurgerClass('burger-bar unclicked')
+      setMenuClass('menu hidden')
+    }
+
+    setMenuClicked(!isMenuClicked)
+
+ }
+
+
+
+
+  return (
+    <div className='Burger'>
+      <nav className='nav'>
+        <div className='burger-menu' onClick={updateMenu}>
+            <div className={burger_class} ></div>
+            <div className={burger_class} ></div>
+            <div className={burger_class} ></div>
+        </div>
+      </nav>
+
+      <div className={menu_class}>   
+          <Nav.Link href='/'>Home</Nav.Link>
+          <Nav.Link href='/about'>About</Nav.Link>
+          <Nav.Link href='/skills'>Skills</Nav.Link>
+          <Nav.Link href='/portfolio'>Portfolio</Nav.Link>
+          <Nav.Link href='/contact'>Contact</Nav.Link>
+      </div>
+
+    </div>
+  )
+}
+
+
+
