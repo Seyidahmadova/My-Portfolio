766aedd19937c9467651b3e1e802d6e5f08fe69e
Aysel Seyidahmadova <romaseyidahmedov@gmail.com>
2023-04-18 18:49:39 +0400

    Fixed Skills page

---
+++ src/Skills/Skills.jsx
@@ -0,0 +1,121 @@
+import React from "react";
+import "./Skills.css";
+
+export default function Skills() {
+  return (
+    <div className="Skills">
+      <div className="skills-container">
+        <div className="skills-header">
+          <h1>Skills</h1>
+        </div>
+        <div className="skills-item-container">
+          <div className="skills-item">
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
+              alt="html5"
+              width="70"
+              height="80"
+            />
+          </div>
+          <div className="skills-item">
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
+              alt="css3"
+              width="70"
+              height="80"
+            />
+          </div>
+          <div className="skills-item">
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
+              alt="javascript"
+              width="70"
+              height="80"
+            />
+          </div>
+          <div className="skills-item">
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg"
+              alt="bootstrap"
+              width="70"
+              height="80"
+            />
+          </div>
+          <div className="skills-item">
+            <img
+              src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg"
+              alt="tailwind"
+              width="70"
+              height="80"
+            />
+          </div>
+          <div className="skills-item">
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"
+              alt="react"
+              width="70"
+              height="80"
+            />
+          </div>
+
+          <div className="skills-item">
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg"
+              alt="nodejs"
+              width="70"
+              height="80"
+            />
+          </div>
+          <div className="skills-item">
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg"
+              alt="express"
+              width="70"
+              height="80"
+            />
+          </div>
+          <div className="skills-item">
+            <img
+              src="https://www.svgrepo.com/show/303229/microsoft-sql-server-logo.svg"
+              alt="mssql"
+              width="70"
+              height="80"
+            />
+          </div>
+          <div className="skills-item">
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg"
+              alt="mysql"
+              width="70"
+              height="80"
+            />
+          </div>
+          <div className="skills-item">
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg"
+              alt="mongodb"
+              width="70"
+              height="80"
+            />
+          </div>
+          <div className="skills-item">
+            <img
+              src="https://www.vectorlogo.zone/logos/figma/figma-icon.svg"
+              alt="figma"
+              width="70"
+              height="80"
+            />
+          </div>
+          <div className="skills-item">
+            <img
+              src="https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg"
+              alt="postman"
+              width="70"
+              height="80"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
